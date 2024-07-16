6a950f5060c46f3f3e5da8fbef81f98223ff3868
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-16 14:37:33 -0400

    Pass all tests

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,4 @@
 class Song
-
   attr_accessor :name, :album, :id
 
   def initialize(name:, album:, id: nil)
@@ -34,13 +33,8 @@ class Song
       VALUES (?, ?)
     SQL
 
-    # insert the song
-    DB[:conn].execute(sql, self.name, self.album)
-
-    # get the song ID from the database and save it to the Ruby instance
+    DB[:conn].execute(sql, [self.name, self.album])
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-
-    # return the Ruby instance
     self
   end
 
@@ -49,4 +43,31 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
